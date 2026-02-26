8d156456b519171de682e9b826989d413b7849a1
luissalazarmvd <luis.salazar@vetadorada.pe>
2026-02-25 20:37:08 -0500

    a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import Script from "next/script";
 import "./globals.css";
 
-export const metadata: Metadata = { title: "SS - Viaje - Estaba aburrido" };
+export const metadata: Metadata = { title: "gays" };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   // Psychosocial ~135 BPM
